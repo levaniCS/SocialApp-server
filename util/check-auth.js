1025709850a823554a4f0571b5be902fb6292e81
levaniCS <levanisarishvili3105@gmail.com>
2020-11-07 15:09:41 +0400

    Fix config inport

--- util/check-auth.js
+++ util/check-auth.js
@@ -1,7 +1,7 @@
 const { AuthenticationError } = require('apollo-server')
 
 const jwt = require('jsonwebtoken')
-const { JWT_SECRET } = require('../config')
+const { JWT_SECRET } = require('../config.js')
 
 module.exports = (context) => {
   const authHeader = context.req.headers.authorization
